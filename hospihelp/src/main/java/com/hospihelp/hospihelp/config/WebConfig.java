303fd1bf233d1fc6b4e7641c8f4c79739bdeed56
Ghinea <ghineadimi@gmail.com>
2026-05-29 12:47:36 +0300

    Fix BOM encoding error in WebConfig

--- hospihelp/src/main/java/com/hospihelp/hospihelp/config/WebConfig.java
+++ hospihelp/src/main/java/com/hospihelp/hospihelp/config/WebConfig.java
@@ -1,4 +1,4 @@
-﻿package com.hospihelp.hospihelp.config;
+package com.hospihelp.hospihelp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
