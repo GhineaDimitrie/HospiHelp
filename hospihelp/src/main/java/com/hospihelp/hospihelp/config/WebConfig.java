8f2a9bf8d08c3a767da046adf320f75f239e027f
Ghinea <ghineadimi@gmail.com>
2026-05-29 12:40:41 +0300

    WebConfig created

---
+++ hospihelp/src/main/java/com/hospihelp/hospihelp/config/WebConfig.java
@@ -0,0 +1,17 @@
+﻿package com.hospihelp.hospihelp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Pentru test, permitem tot. Ulterior poți pune link-ul de frontend.
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
